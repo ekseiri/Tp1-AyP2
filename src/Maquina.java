27c723d8fcd40cb222b1c2cf7cd9a3246755dd8d
cybercatnet <cybercat_666_78@hotmail.com>
2015-04-17 06:39:26 -0300

    Creado Class Maquina

---
+++ src/Maquina.java
@@ -0,0 +1,38 @@
+import utils.Cola;
+import utils.ColaException;
+
+public class Maquina
+{
+	Cola cola;
+	Auto auto;
+
+	public Maquina()
+	{
+		cola = new Cola();
+	}
+
+	boolean estaVacia()
+	{
+		return auto == null;
+	}
+
+	void nextAuto()
+	{
+		try
+		{
+			this.auto= (Auto) this.cola.desencolar(); //no se si es una negrada esto, mepa que si
+		}
+		catch (ColaException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	Auto sacarAuto()
+	{
+		Auto aux=new Auto(); //revisar si no es otra villereada
+		this.auto=null;
+		return aux;
+	}
+
+}
