2739b4bead53f026f6ecd2b9b6374a6b698829c7
Emiliano Kseiri <ekseiriar@gmail.com>
2015-04-17 17:14:05 -0300

    Documentacion

--- src/Maquina.java
+++ src/Maquina.java
@@ -6,39 +6,50 @@ public class Maquina
 	private Cola cola;
 	private Auto auto;
 	private Servicio servicio;
-
+    
+    /**
+     * Crea una nueva Maquina, con su Cola asociada
+     */
 	public Maquina()
 	{
 		cola = new Cola();
 	}
-	
+    
+    /**
+     * Devuelve true si no hay ingresado en la maquina
+     */
 	public boolean estaVacia()
 	{
 		return auto == null;
 	}
-
+    
+    /**
+     * Saca el primer auto de la cola y lo ingresa a la maquina
+     */
 	public void nextAuto()
 	{
 		
 		//no se si es una negrada esto, mepa que si
-		try
-		{
-			this.auto = (Auto) this.cola.desencolar();
-		}
-		catch (ColaException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-			
+        try
+        {
+            this.auto = (Auto) this.cola.desencolar();
+        }
+        catch (ColaException e)
+        {
+            this.auto = null;
+        } 
 	}
 	
+    /**
+     * Retira el auto de la maquina y lo pasa al siguiente estado
+     */
 	public Auto sacarAuto()
 	{
-		Auto aux=this.auto; //revisar si no es otra villereada
+
+		Auto auto = this.auto; //revisar si no es otra villereada
 		this.auto=null;
 		
-		return aux;
+		return auto;
 	}
 	
 	public void avanzarCola(Maquina proximaMaquina)
