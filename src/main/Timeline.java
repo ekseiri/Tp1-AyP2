876f1b2711200ba9428371b51bd5ab100d6d9440
cybercatnet <cybercat_666_78@hotmail.com>
2015-04-18 01:40:46 -0300

    PriorityQueue Timeline

--- src/main/Timeline.java
+++ src/main/Timeline.java
@@ -1,9 +1,22 @@
 package main;
 
+import java.util.PriorityQueue;
+
+import events.Evento;
+import utils.Comparador;
+
 public class Timeline
 {
+	private PriorityQueue timeline;
 	private int horarioActual;
-	
+	private Comparador comparador;
+
+	public Timeline()
+	{
+		this.horarioActual = 0;
+		timeline = new PriorityQueue(comparador);
+	}
+
 	public int getHorarioActual()
 	{
 		return horarioActual;
