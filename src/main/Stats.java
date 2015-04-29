09c14786768cdc680e6cb957f123caa1066c6eb5
cybercatnet <cybercat_666_78@hotmail.com>
2015-04-29 00:12:25 -0300

    printCostoPorDiaYPorServicio

--- src/main/Stats.java
+++ src/main/Stats.java
@@ -3,26 +3,28 @@ package main;
 public class Stats {
     private int[] cantidadServicios = new int[4];
     private int[] acumuladorTiempoEnCola = new int[2];
-    
+
     /**
-     * Crea el contenedor de estadisticas, inicializando la cantidad
-     * de servicios y el acumulador de tiempo para cada cola en 0
+     * Crea el contenedor de estadisticas, inicializando la cantidad de
+     * servicios y el acumulador de tiempo para cada cola en 0
      */
-    public Stats(){
-	
+    public Stats() {
+
 	for (int i = 0; i < this.cantidadServicios.length; i++)
 	    this.cantidadServicios[i] = 0;
-	
+
 	for (int i = 0; i < this.cantidadServicios.length; i++)
 	    this.acumuladorTiempoEnCola[i] = 0;
-	
+
     }
-    
+
     /**
      * Suma 1 a la cantidad de servicios realizados para el servicio indicado
-     * @param tipoDeServicio TipoDeServicio requerido
+     * 
+     * @param tipoDeServicio
+     *            TipoDeServicio requerido
      */
-    public void addServicio(TipoDeServicio tipoDeServicio){
+    public void addServicio(TipoDeServicio tipoDeServicio) {
 	if (tipoDeServicio == TipoDeServicio.ECONOMICO)
 	    this.cantidadServicios[0]++;
 	if (tipoDeServicio == TipoDeServicio.COMPLETO)
@@ -31,14 +33,17 @@ public class Stats {
 	    this.cantidadServicios[2]++;
 	if (tipoDeServicio == TipoDeServicio.ENCERADO)
 	    this.cantidadServicios[3]++;
+
     }
-    
+
     /**
      * Devuelve la cantidad de Servicios realizados del tipo indicado
-     * @param tipoDeServicio TipoDeServicio requerido
+     * 
+     * @param tipoDeServicio
+     *            TipoDeServicio requerido
      * @return -1 Si el TipoDeServicio es inexistente
      */
-    public int getServicios(TipoDeServicio tipoDeServicio){
+    public int getServicios(TipoDeServicio tipoDeServicio) {
 	if (tipoDeServicio == TipoDeServicio.ECONOMICO)
 	    return this.cantidadServicios[0];
 	if (tipoDeServicio == TipoDeServicio.COMPLETO)
@@ -47,50 +52,82 @@ public class Stats {
 	    return this.cantidadServicios[2];
 	if (tipoDeServicio == TipoDeServicio.ENCERADO)
 	    return this.cantidadServicios[3];
-	
+
 	return -1;
     }
-    
+
     /**
      * Suma tiempo de espera en la cola indicada
-     * @param tipoDeServicio TipoDeServicio requerido
+     * 
+     * @param tipoDeServicio
+     *            TipoDeServicio requerido
      */
-    public void addTiempoEnCola(TipoDeServicio tipoDeServicio){
-	if ((tipoDeServicio == TipoDeServicio.ECONOMICO) || (tipoDeServicio == TipoDeServicio.COMPLETO)
-		 || (tipoDeServicio == TipoDeServicio.PREMIUM))
+    public void addTiempoEnCola(TipoDeServicio tipoDeServicio) {
+	if ((tipoDeServicio == TipoDeServicio.ECONOMICO)
+		|| (tipoDeServicio == TipoDeServicio.COMPLETO)
+		|| (tipoDeServicio == TipoDeServicio.PREMIUM))
 	    this.acumuladorTiempoEnCola[0]++;
-	
+
 	if (tipoDeServicio == TipoDeServicio.ENCERADO)
 	    this.acumuladorTiempoEnCola[1]++;
     }
-    
+
     /**
      * Devuelve el tiempo de espera acumulado en la cola indicada
-     * @param tipoDeServicio TipoDeServicio requerido
-     * @return -1 Si el TipoDeServicio es inexistente 
+     * 
+     * @param tipoDeServicio
+     *            TipoDeServicio requerido
+     * @return -1 Si el TipoDeServicio es inexistente
      */
-    public int getTiempoEnCola(TipoDeServicio tipoDeServicio){
-	if ((tipoDeServicio == TipoDeServicio.ECONOMICO) || (tipoDeServicio == TipoDeServicio.COMPLETO)
-		 || (tipoDeServicio == TipoDeServicio.PREMIUM))
+    public int getTiempoEnCola(TipoDeServicio tipoDeServicio) {
+	if ((tipoDeServicio == TipoDeServicio.ECONOMICO)
+		|| (tipoDeServicio == TipoDeServicio.COMPLETO)
+		|| (tipoDeServicio == TipoDeServicio.PREMIUM))
 	    return this.acumuladorTiempoEnCola[0];
-	
+
 	if (tipoDeServicio == TipoDeServicio.ENCERADO)
 	    return this.acumuladorTiempoEnCola[1];
-	
+
 	return -1;
     }
-    
+
     /**
      * Setea en 0 el tiempo de espera para la cola indicada
-     * @param tipoDeServicio TipoDeServicio requerido
+     * 
+     * @param tipoDeServicio
+     *            TipoDeServicio requerido
      */
-    public void resetTiempoEnCola(TipoDeServicio tipoDeServicio){
-	if ((tipoDeServicio == TipoDeServicio.ECONOMICO) || (tipoDeServicio == TipoDeServicio.COMPLETO)
-		 || (tipoDeServicio == TipoDeServicio.PREMIUM))
+    public void resetTiempoEnCola(TipoDeServicio tipoDeServicio) {
+	if ((tipoDeServicio == TipoDeServicio.ECONOMICO)
+		|| (tipoDeServicio == TipoDeServicio.COMPLETO)
+		|| (tipoDeServicio == TipoDeServicio.PREMIUM))
 	    this.acumuladorTiempoEnCola[0] = 0;
-	
+
 	if (tipoDeServicio == TipoDeServicio.ENCERADO)
 	    this.acumuladorTiempoEnCola[1] = 0;
     }
-    
+
+    public void printCostoPorDiaYPorServicio() {
+	double costoEconomicos = getServicios(TipoDeServicio.ECONOMICO)
+		* Main.economico.getCosto();
+	double costoCompletos = getServicios(TipoDeServicio.COMPLETO)
+		* Main.completo.getCosto();
+	double costoPremiums = getServicios(TipoDeServicio.PREMIUM)
+		* Main.premium.getCosto();
+	double costoEncerados = getServicios(TipoDeServicio.ENCERADO)
+		* Main.encerado.getCosto();
+
+	System.out.println("Costo por Servicio:");
+	System.out.println();
+	System.out.println("Económico: " + costoEconomicos);
+	System.out.println("Completo: " + costoCompletos);
+	System.out.println("Premium: " + costoPremiums);
+	System.out.println("Encerado: " + costoEncerados);
+	System.out.println();
+	System.out
+		.println("Costo Total: "
+			+ (costoEconomicos + costoCompletos + costoPremiums + costoEncerados));
+	System.out.println();
+    }
+
 }
